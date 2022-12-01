7d171fa15e2512594feef4f9b307aac5cd9df99c
Marcus Ong <markuz5116@gmail.com>
2022-12-01 20:42:36 +0800

    Create Video class

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "tim": {
-        "age": 19,
-        "gender": "male"
-    },
-    "mary": {
-        "age": 20,
-        "gender": "female"
-    }
-}
+videos = {}
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+    def put(self, video_id):
+        return {}
+
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
